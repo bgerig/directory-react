c51b8b35be093a67ed02125df5801a12db8a157a
Alex Gerig <alex.gerig@carsforsale.com>
2021-09-23 12:00:35 -0500

    Clean up and format actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,33 +1,24 @@
-import { firebase } from '../firebase/firebase';
+import { firebase } from "../firebase/firebase";
 
-// login action
 export const login = (uid) => ({
-	type: 'LOGIN',
-	uid: uid
-})
+    type: "LOGIN",
+    uid: uid,
+});
 
-// Action that starts the login process
 export const startLogin = (email, password) => {
-	return () => {
-		// we use 'return' so this function returns a promise chain
-		return firebase.auth().signInWithEmailAndPassword(email, password);
-		
-		// TO USE GOOGLE ACCOUNT SIGN IN INSTEAD OF EMAIL/PASSWORD
-		// // we use 'return' so this function returns a promise chain so we can use it on our test cases
-		// return firebase.auth().signInWithPopup(googleAuthProvider);//handles the authentication process using google as the login provider
-	}
-}
+    return () => {
+        // we use 'return' so it returns a promise chain
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+    };
+};
 
-// logout action
 export const logout = () => ({
-	type: 'LOGOUT'
-})
+    type: "LOGOUT",
+});
 
-// Action that starts the logout process
 export const startLogout = () => {
-	return () => {
-		
-		// we use 'return' so this function returns a promise chain so we can use it on our test cases
-		return firebase.auth().signOut();
-	}
-}
+    return () => {
+        // we use 'return' so it returns a promise chain
+        return firebase.auth().signOut();
+    };
+};
