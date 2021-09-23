447b1c958ccf5fe7e011e4db196b0889fc9565ce
Alex Gerig <alex.gerig@carsforsale.com>
2021-09-23 11:05:53 -0500

    Clean up EntryListItem

--- src/components/EntryListItem.jsx
+++ src/components/EntryListItem.jsx
@@ -1,96 +1,94 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { startRemoveEntry } from '../actions/entries';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import defaultPhoto from '../assets/images/default-photo.jpg';
-// import { forEach } from '@firebase/util';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { startRemoveEntry } from "../actions/entries";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import defaultPhoto from "../assets/images/default-photo.jpg";
 
-class EntryListItem extends Component {  //we destructure the props object being passed
-	constructor(props) {
-		super(props);
+const EntryListItem = (props) => {
+    const { isAuthenticated, entry, dispatch } = props;
+    const imageUrl = entry.imageUrl ? entry.imageUrl : defaultPhoto;
 
-		this.state = {
-			entry: props.entry,
-			dispatch: props.dispatch
-		}
-	}
+    const deleteEntry = () => {
+        if (window.confirm("Are you sure you want to delete this entry?")) {
+            dispatch(startRemoveEntry({ id: entry.id, imageId: entry.imageId }));
+        }
+    };
 
-	deleteEntry = () => {
-		if (window.confirm('Are you sure you want to delete this entry?')) {
-			this.state.dispatch(startRemoveEntry({ id: this.state.entry.id, imageId: this.state.entry.imageId }));
-		}
-	}
+    return (
+        <div className="entry-list-item">
+            <NavLink to={`/details/${entry.id}`}>
+                <div className="entry-list-item__image" style={{ backgroundImage: "url(" + imageUrl + ")" }}></div>
+            </NavLink>
 
-	render() {
-		return (
-			<div className="entry-list-item">
+            {entry.entryType === "person" && (
+                <div className="entry-list-item__person">
+                    <div className="entry-list-item__name">
+                        <h3>
+                            <NavLink to={`/details/${entry.id}`}>
+                                {entry.firstName} {entry.lastName}
+                            </NavLink>
+                        </h3>
 
-				<NavLink to={`/details/${this.state.entry.id}`} >
-					{this.state.entry.imageUrl ?
-						<div className="entry-list-item__image" style={{ backgroundImage: "url(" + this.state.entry.imageUrl + ")" }}></div> :
-						<div className="entry-list-item__image" style={{ backgroundImage: "url(" + defaultPhoto + ")" }}></div>
-					}
-				</NavLink>
+                        {entry.team.map((team, index) => {
+                            return (
+                                <NavLink to={`/teams/${team.value}`} key={index}>
+                                    <span className={`entry-list-item__badge entry-list-item__badge--${team.value}`}>{team.value}</span>
+                                </NavLink>
+                            );
+                        })}
+                    </div>
+                    <p className="entry-list-item__position">{entry.position.toUpperCase()}</p>
+                </div>
+            )}
 
-				{this.state.entry.entryType === "person" &&
-					<div className="entry-list-item__person">
-						<div className="entry-list-item__name">
-							<h3><NavLink to={`/details/${this.state.entry.id}`} >{this.state.entry.firstName} {this.state.entry.lastName}</NavLink></h3>
+            {entry.entryType === "room" && (
+                <div className="entry-list-item__room">
+                    <h3>
+                        <NavLink to={`/details/${entry.id}`}>{entry.roomName}</NavLink>
+                    </h3>
+                </div>
+            )}
 
-							{/* Loops through entry.team array */}
-							{this.state.entry.team.map((team, index) => {
-								return <NavLink to={`/teams/${team.value}`} key={index} ><span className={`entry-list-item__badge entry-list-item__badge--${team.value}`}>{team.value}</span></NavLink>
-							})}
-						</div>
-						<p className="entry-list-item__position">{this.state.entry.position.toUpperCase()}</p>
-					</div>
-				}
+            {entry.entryType === "other" && (
+                <div className="entry-list-item__room">
+                    <h3>
+                        <NavLink to={`/details/${entry.id}`}>{entry.otherName}</NavLink>
+                    </h3>
+                </div>
+            )}
 
-				{this.state.entry.entryType === "room" &&
-					<div className="entry-list-item__room">
-						<h3><NavLink to={`/details/${this.state.entry.id}`} >{this.state.entry.roomName}</NavLink></h3>
-					</div>
-				}
+            {entry.phoneExtension && (
+                <div className="entry-list-item__extension">
+                    {entry.entryType === "other" && <h3>Ext. {entry.phoneExtension}</h3>}
+                    {entry.entryType !== "other" && (
+                        <h3>
+                            <a href={`tel:555123${entry.phoneExtension}`}>Ext. {entry.phoneExtension}</a>
+                        </h3>
+                    )}
+                </div>
+            )}
 
-				{this.state.entry.entryType === "other" &&
-					<div className="entry-list-item__room">
-						<h3><NavLink to={`/details/${this.state.entry.id}`} >{this.state.entry.otherName}</NavLink></h3>
-					</div>
-				}
-
-				{this.state.entry.phoneExtension &&
-					<div className="entry-list-item__extension">
-						{this.state.entry.entryType === "other" &&
-							<h3>Ext. {this.state.entry.phoneExtension}</h3>
-						}
-						{this.state.entry.entryType !== "other" &&
-							<h3><a href={`tel:555123${this.state.entry.phoneExtension}`}>Ext. {this.state.entry.phoneExtension}</a></h3>
-						}
-					</div>
-				}
-
-				{/* Renders edit and delete button if users is authenticated */}
-				{this.props.isAuthenticated &&
-					<div className="entry-list-item__control">
-						<span><NavLink to={`/edit/${this.state.entry.id}`} ><FontAwesomeIcon icon="edit" /></NavLink></span>
-						<span className="entry-list-item__remove" onClick={this.deleteEntry}>
-							<FontAwesomeIcon icon="trash-alt" />
-						</span>
-					</div>
-				}
-			</div>
-		)
-	}
+            {isAuthenticated && (
+                <div className="entry-list-item__control">
+                    <span>
+                        <NavLink to={`/edit/${entry.id}`}>
+                            <FontAwesomeIcon icon="edit" />
+                        </NavLink>
+                    </span>
+                    <span className="entry-list-item__remove" onClick={deleteEntry}>
+                        <FontAwesomeIcon icon="trash-alt" />
+                    </span>
+                </div>
+            )}
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
-	return {
-		isAuthenticated: !!state.auth.uid
-	}
-}
+    return {
+        isAuthenticated: !!state.auth.uid,
+    };
+};
 
-// connect() passes dispatc() as a prop
-const EntryListItemConnected = connect(mapStateToProps)(EntryListItem);
-
-export default EntryListItemConnected;
+export default connect(mapStateToProps)(EntryListItem);
